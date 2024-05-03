0e793bf0277f61533c0c6bb55b7960c22406ca37
tranthanhphat248842 <168280461+tranthanhphat248842@users.noreply.github.com>
2024-05-03 14:30:08 +0700

    Cau1.C

---
+++ Cau1.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    printf("So nguyen co 2 chu so va boi so la 7:\n");
+    for (i = 14 ; i< 100; i+=7)
+    {
+        printf("%d ",i);
+    }
+    printf("\n");
+    return 0;
+}
